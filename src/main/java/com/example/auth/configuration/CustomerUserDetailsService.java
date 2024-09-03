92cdd1812c445e159675aa3cf7a8ffd57c15a991
Konrad <syrnikkonrad@gmail.com>
2024-09-03 19:41:19 +0200

    add authentication provider

--- src/main/java/com/example/auth/configuration/CustomerUserDetailsService.java
+++ src/main/java/com/example/auth/configuration/CustomerUserDetailsService.java
@@ -1,4 +1,27 @@
 package com.example.auth.configuration;
 
-public class CustomerUserDetailsService {
+import com.example.auth.entity.User;
+import com.example.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class CustomerUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomerUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findUserByLogin(username);
+        return user.map(CustomerUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with name: " + username));
+    }
 }
